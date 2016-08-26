0bad70fcde04d029eebaa8b1c30b899d4092c576
webkaz <web.kaz@gmail.com>
2016-08-26 19:55:50 +0900

    DBを集約

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,11 +1,13 @@
 docker-compose \
 -p DevTools \
 -f nginx-proxy/docker-compose.yml \
--f ourcloud/docker-compose.yml \
+-f mariadb/docker-compose.yml \
+-f testlink/docker-compose.yml \
 -f redmine/docker-compose.yml \
+-f gitlab/docker-compose.yml \
+-f ourcloud/docker-compose.yml \
 -f gitbucket/docker-compose.yml \
 -f jenkins/docker-compose.yml \
 -f Rocket.Chat/docker-compose.yml \
--f testlink/docker-compose.yml \
 up -d \
-#-f gitlab/docker-compose.yml \
+
